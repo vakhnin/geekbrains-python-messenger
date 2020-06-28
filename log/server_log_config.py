4c3b05cc54c287c9110ce472e8f4bdb8924069a8
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2020-06-28 11:07:00 +0300

    Изменен формат записей в логе

--- log/server_log_config.py
+++ log/server_log_config.py
@@ -3,7 +3,8 @@ import os
 
 LOG = logging.getLogger('messenger.server')
 
-FORMATTER = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s ')
+FORMATTER = \
+    logging.Formatter('%(asctime)s - %(levelname)s -  %(name)s - %(message)s ')
 
 logs_path = os.path.join(os.getcwd(), 'log', 'logs')
 if not os.path.exists(logs_path):
