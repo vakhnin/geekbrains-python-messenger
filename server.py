c4051944a205ae202c0041bc7243167f273b60de
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2020-06-29 12:00:08 +0300

    Журналирование исключений и вызовов функций на сервере

--- server.py
+++ server.py
@@ -9,6 +9,15 @@ from common.variables import (BROKEN_JIM, DEFAULT_PORT, ENCODING,
 from log.server_log_config import LOG
 
 
+def log(func):
+    def wrap_log(*args, **kwargs):
+        LOG.debug(f'Вызов функции: {func.__name__} с аргументами :{args}')
+        return func(*args, **kwargs)
+
+    return wrap_log
+
+
+@log
 def make_listen_socket():
     parser = argparse.ArgumentParser()
     parser.add_argument('-a', default='')
@@ -21,6 +30,7 @@ def make_listen_socket():
     return sock
 
 
+@log
 def parse_received_bytes(data):
     if not isinstance(data, bytes):
         return NOT_BYTES
@@ -34,9 +44,11 @@ def parse_received_bytes(data):
             return NO_TIME
         return jim_obj
     except json.JSONDecodeError:
+        LOG.error(BROKEN_JIM)
         return BROKEN_JIM
 
 
+@log
 def choice_jim_action(jim_obj):
     if jim_obj == NOT_BYTES:
         return make_answer(500, {})
@@ -49,6 +61,7 @@ def choice_jim_action(jim_obj):
             return make_answer(400, {'error': UNKNOWN_ACTION})
 
 
+@log
 def make_answer(code, message={}):
     answer = {'response': code}
     if 'error' in message.keys():
@@ -58,6 +71,7 @@ def make_answer(code, message={}):
     return answer
 
 
+@log
 def parse_presence(jim_obj):
     if 'user' not in jim_obj.keys():
         return make_answer(400, {'error': 'Request has no "user"'})
@@ -77,7 +91,7 @@ def parse_presence(jim_obj):
 
 
 def main():
-    LOG.debug('Старт приложения')
+    LOG.debug('Старт сервера')
     sock = make_listen_socket()
     while True:
         try:
@@ -95,15 +109,16 @@ def main():
                 except ConnectionResetError:
                     err_msg = 'Удаленный хост принудительно разорвал ' + \
                         'существующее подключение'
-                    print(err_msg)
+                    LOG.error(err_msg)
                     conn.close()
                 except Exception as e:
-                    print(f'Unknown error "{e}"')
+                    LOG.error(f'Unknown error "{e}"')
+                    conn.close()
         except KeyboardInterrupt:
-            print('Canceled by keyboard')
-            exit(1)
+            LOG.debug('Canceled by keyboard')
+            exit(0)
         except Exception as e:
-            print(f'Unknown error "{e}"')
+            LOG.error(f'Unknown error "{e}"')
             exit(1)
         conn.close()
 
