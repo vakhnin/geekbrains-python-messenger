ffd5d166eb89494cf9fcc125a392519c9b3a7104
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2020-07-11 15:36:10 +0300

    Переименование папки

---
+++ lesson7_2/common_client.py
@@ -0,0 +1,63 @@
+import json
+import sys
+from datetime import datetime
+from socket import SOCK_STREAM, socket
+
+from common.decorators import log
+from common.variables import (DEFAULT_IP_ADDRESS, DEFAULT_PORT, ENCODING,
+                              MAX_PACKAGE_LENGTH)
+from log.client_log_config import LOG
+
+
+@log(LOG)
+def parse_args():
+    addr, port = DEFAULT_IP_ADDRESS, DEFAULT_PORT
+    if len(sys.argv) > 1:
+        addr = sys.argv[1]
+    if len(sys.argv) > 2:
+        port = int(sys.argv[2])
+
+    sock = socket(type=SOCK_STREAM)
+    sock.connect((addr, port))
+
+    return addr, port
+
+
+@log(LOG)
+def parse_answer(jim_obj):
+    if not isinstance(jim_obj, dict):
+        print('Server answer not dict')
+        return
+    if 'response' in jim_obj.keys():
+        print(f'Server answer: {jim_obj["response"]}')
+    else:
+        print('Answer has not "response" code')
+    if 'error' in jim_obj.keys():
+        print(f'Server error message: {jim_obj["error"]}')
+    if 'alert' in jim_obj.keys():
+        print(f'Server alert message: {jim_obj["alert"]}')
+
+
+@log(LOG)
+def make_presence_message(account_name, status):
+    return {
+        'action': 'presence',
+        'time': datetime.now().timestamp(),
+        'type': 'status',
+        'user': {
+            'account_name': account_name,
+            'status': status,
+        }
+    }
+
+
+@log(LOG)
+def send_message_take_answer(sock, msg):
+    msg = json.dumps(msg, separators=(',', ':'))
+    try:
+        sock.send(msg.encode(ENCODING))
+        data = sock.recv(MAX_PACKAGE_LENGTH)
+        return json.loads(data.decode(ENCODING))
+    except json.JSONDecodeError:
+        LOG.error('Answer JSON broken')
+        return {}
--- lesson7_2/read_client.py
+++ lesson7_2/read_client.py
@@ -1,15 +1,16 @@
 from socket import AF_INET, SOCK_STREAM, socket
 
-ADDRESS = ('localhost', 7777)
+from lesson7_2.common_client import parse_args
 
 
-def read_client():
+def main():
+    addr, port = parse_args()
     with socket(AF_INET, SOCK_STREAM) as sock:
-        sock.connect(ADDRESS)
+        sock.connect((addr, port))
         while True:
             data = sock.recv(1024).decode('utf-8')
             print(data)
 
 
 if __name__ == '__main__':
-    read_client()
+    main()
