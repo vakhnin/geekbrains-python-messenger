1e6607d7a5f0c09501a78df55aee7da1fc7f6720
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2020-07-23 16:28:44 +0300

    Удаление файлов с уроками

--- common/decorators.py
+++
@@ -1,8 +0,0 @@
-def log(logger):
-    def decorator(func):
-        def decorated(*args, **kwargs):
-            res = func(*args, **kwargs)
-            logger.debug(f'Log: {func.__name__}({args},{kwargs}) = {res}')
-            return res
-        return decorated
-    return decorator
